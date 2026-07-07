2d175b902a8f20cd282327fca0ec0d5d8487bef4
dadt25 <asdorqwe@gmail.com>
2026-07-07 04:36:12 -0400

    Update freecam with worker loop

--- freecam.h
+++ freecam.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <atomic>
 #include <cmath>
+#include <thread>
+#include <chrono>
 #include "../overlay/utils/W2S.h"
 #include "../overlay/imgui/imgui.h"
 #include "../overlay/imgui/KeyBind.h"
@@ -13,6 +15,7 @@ namespace FreecamRuntime
     inline Vectors::Vector3 CameraPosition{ 0.f, 0.f, 0.f };
     inline Vectors::Vector3 CameraRotation{ 0.f, 0.f, 0.f };
     inline float CameraSpeed{ 50.f };
+    inline std::mutex UpdateMutex;
 }
 
 inline bool IsFreecamKeyActive()
@@ -48,39 +51,41 @@ inline void UpdateFreecamPosition()
 
     FreecamRuntime::Enabled.store(true);
 
-    const auto workspace = Globals::Roblox::DataModel.FindFirstChild("Workspace");
-    if (!workspace.address)
-        return;
-
-    const auto camera = workspace.FindFirstChild("Camera");
-    if (!camera.address)
-        return;
-
-    // Get keyboard input for movement
-    float moveX = 0.f, moveY = 0.f, moveZ = 0.f;
-
-    if (GetAsyncKeyState('W') & 0x8000)
-        moveZ += FreecamRuntime::CameraSpeed;
-    if (GetAsyncKeyState('S') & 0x8000)
-        moveZ -= FreecamRuntime::CameraSpeed;
-    if (GetAsyncKeyState('D') & 0x8000)
-        moveX += FreecamRuntime::CameraSpeed;
-    if (GetAsyncKeyState('A') & 0x8000)
-        moveX -= FreecamRuntime::CameraSpeed;
-    if (GetAsyncKeyState(VK_SPACE) & 0x8000)
-        moveY += FreecamRuntime::CameraSpeed;
-    if (GetAsyncKeyState(VK_CONTROL) & 0x8000)
-        moveY -= FreecamRuntime::CameraSpeed;
-
-    // Update camera position
     try
     {
+        const auto workspace = Globals::Roblox::DataModel.FindFirstChild("Workspace");
+        if (!workspace.address)
+            return;
+
+        const auto camera = workspace.FindFirstChild("Camera");
+        if (!camera.address)
+            return;
+
+        // Get keyboard input for movement
+        float moveX = 0.f, moveY = 0.f, moveZ = 0.f;
+
+        if (GetAsyncKeyState('W') & 0x8000)
+            moveZ += FreecamRuntime::CameraSpeed;
+        if (GetAsyncKeyState('S') & 0x8000)
+            moveZ -= FreecamRuntime::CameraSpeed;
+        if (GetAsyncKeyState('D') & 0x8000)
+            moveX += FreecamRuntime::CameraSpeed;
+        if (GetAsyncKeyState('A') & 0x8000)
+            moveX -= FreecamRuntime::CameraSpeed;
+        if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+            moveY += FreecamRuntime::CameraSpeed;
+        if (GetAsyncKeyState(VK_CONTROL) & 0x8000)
+            moveY -= FreecamRuntime::CameraSpeed;
+
+        // Update camera position
         Vectors::Vector3 currentPos = camera.Position();
-        currentPos.x += moveX * 0.016f; // Delta time adjustment
+        currentPos.x += moveX * 0.016f;
         currentPos.y += moveY * 0.016f;
         currentPos.z += moveZ * 0.016f;
 
         camera.SetPosition(currentPos);
+        
+        std::lock_guard<std::mutex> lock(FreecamRuntime::UpdateMutex);
         FreecamRuntime::CameraPosition = currentPos;
     }
     catch (...)
@@ -101,10 +106,24 @@ inline void RunFreecam(ImDrawList* drawList)
 
     if (Options::Freecam::ShowInfo && FreecamRuntime::Enabled.load())
     {
+        std::lock_guard<std::mutex> lock(FreecamRuntime::UpdateMutex);
         const std::string info = "Freecam: " 
             + std::to_string(static_cast<int>(FreecamRuntime::CameraPosition.x)) + ", "
             + std::to_string(static_cast<int>(FreecamRuntime::CameraPosition.y)) + ", "
             + std::to_string(static_cast<int>(FreecamRuntime::CameraPosition.z));
         drawList->AddText(ImVec2(10.f, 50.f), IM_COL32(255, 255, 0, 255), info.c_str());
+    }
+}
+
+inline void RunFreecamLoop()
+{
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+        
+        if (!Options::Freecam::Enabled)
+            continue;
+
+        UpdateFreecamPosition();
     }
 }
